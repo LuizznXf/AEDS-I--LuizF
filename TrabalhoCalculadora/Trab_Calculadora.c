9ffb912534e5147cf264fe308f46efdc10e67bba
Luiz Fernando Ferreira De Matos <0116349@ifmg.edu.br>
2025-10-23 09:47:55 -0300

    Ajuste na Logica e nova função

--- TrabalhoCalculadora/Trab_Calculadora.c
+++ TrabalhoCalculadora/Trab_Calculadora.c
@@ -28,16 +28,6 @@ float Divisao(float n1, float n2) {
     return n1 / n2;
 }
 
-int verificarOpcao(int opcao) {
-    if (opcao != 1 && opcao != 6) {
-        printf("Escolha a opcao 1 para iniciarmos ou 6 pra fechar a Calculadora\n");
-        sleep(3);
-        system("cls");
-        return 1;
-    }
-    return 0;
-}
-
 int main() {
     float n1 = 0, n2 = 0, retorno = 0;
     int opcao = 0;
@@ -50,8 +40,6 @@ int main() {
         printf("6--SAIR\n");
         scanf("%d", &opcao);
 
-        verificarOpcao(opcao);
-
         if (opcao == 1) {
             printf("INFORME OS NUMEROS:\n");
             scanf("%f%f", &n1, &n2);
@@ -61,16 +49,17 @@ int main() {
             printf("3--SUBTRACAO\n");
             printf("4--MULTIPLICACAO\n");
             printf("5--DIVISAO\n");
-            printf("\n6--SAIR\n");
             scanf("%d", &opcao);
         }
-    
+
         switch (opcao) {
         case 2:
             if (z == 1) {
                 retorno = Soma(n1, n2);
                 system("cls");
                 printf("Resultado: %.2f\n", retorno);
+            } else {
+                printf("Escolha a opcao 1 para iniciarmos ou 6 pra fechar a Calculadora\n");
             }
             break;
         case 3:
@@ -78,13 +67,17 @@ int main() {
                 retorno = Subtracao(n1, n2);
                 system("cls");
                 printf("Resultado: %.2f\n", retorno);
-             }
+            } else {
+                printf("Escolha a opcao 1 para iniciarmos ou 6 pra fechar a Calculadora\n");
+            }
             break;
         case 4:
             if (z == 1) {
                 retorno = Multiplicacao(n1, n2);
                 system("cls");
                 printf("Resultado: %.2f\n", retorno);
+            } else {
+                printf("Escolha a opcao 1 para iniciarmos ou 6 pra fechar a Calculadora\n");
             }
             break;
         case 5:
@@ -94,16 +87,17 @@ int main() {
                     system("cls");
                     printf("Resultado: %.2f\n", retorno);
                 }
+            } else {
+                printf("Escolha a opcao 1 para iniciarmos ou 6 pra fechar a Calculadora\n");
             }
             break;
         case 6:
             printf("SAINDO...\n");
             break;
         default:
-         if(z==1){
-            printf("Opcao invalida!\n");
-         }
-        break;
+            if (opcao != 1)
+                printf("OPCAO INVALIDA!\n");
+            break;
         }
 
     } while (opcao != 6);
