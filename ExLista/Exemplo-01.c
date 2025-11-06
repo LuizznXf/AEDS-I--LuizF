b9e59cf1a35645459b1d31304580a587bfe4edb0
Luiz Fernando Ferreira De Matos <0116349@ifmg.edu.br>
2025-11-06 11:21:41 -0300

    Acrescimo dos itens 5 e função do mesmo

--- ExLista/Exemplo-01.c
+++ ExLista/Exemplo-01.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 #define InicioArranjo 1
 #define MaxTam 5
 
@@ -40,28 +40,50 @@ void Insere(TipoItem x, TipoLista *Lista)
         printf("Erro: Lista cheia\n");
         return;
     }
-    else{
-    Lista->Item[Lista->Ultimo] = x;
-    Lista->Ultimo++;
+    else
+    {
+        Lista->Item[Lista->Ultimo] = x;
+        Lista->Ultimo++;
     }
 }
 
 void Imprime(TipoLista Lista)
 {
     int aux;
-    for(aux = Lista.Primeiro; aux < Lista.Ultimo; aux++)
+    for (aux = Lista.Primeiro; aux < Lista.Ultimo; aux++)
     {
         printf("Codigo: %d\n", Lista.Item[aux].cod);
         printf("Idade: %d\n", Lista.Item[aux].idade);
     }
 }
 
+void Procura(TipoItem x, TipoLista Lista, Apontador *Pos)
+{
+    int i;
+    int encontrado = 0;
+    for (i = Lista.Primeiro; i <=Lista.Ultimo-2; i++)
+    {
+        if (Lista.Item[i].cod == x.cod)
+        {
+            encontrado = 1;
+            printf("Elemento encontrado: Codigo na posicao %d\n", i);
+            break;
+        }
+    }
+    if (encontrado == 0)
+    {
+       printf("Elemento nao encontrado na lista\n");
+    }
+    
+}
+
 int main()
 {
-   int opcao,resposta;
-    setlocale(LC_ALL,"");
+    int opcao, resposta;
+    setlocale(LC_ALL, "");
     FLVazia(&Lista);
-    do{
+    do
+    {
 
         system("cls");
         printf("\n\nMENU\n");
@@ -71,38 +93,42 @@ int main()
         printf("2 - Verificar se a lista esta vazia\n");
         printf("3 - Inserir elemento na lista\n");
         printf("4 - Imprimir os elemento da lista\n");
-        scanf("%d",&opcao);
-        switch(opcao)
+        printf("5 - Pesquisar elemento na lista\n");
+        scanf("%d", &opcao);
+        switch (opcao)
         {
-           case 0:
+        case 0:
             printf("Saindo...\n");
-           break;
-           case 1:
-            FLVazia(&Lista);
-               printf("Lista esvaziada com sucesso!\n");
             break;
-            case 2:
-             resposta=Vazia(Lista);
-                if(resposta==1)
-                    printf("A lista esta vazia!\n");
-                else
-                    printf("A lista nao esta vazia!\n");
-                break;
-            case 3:
-                printf("Digite o codigo\n");
-                scanf("%d", &x.cod);
-                printf("Digite a idade\n");
-                scanf("%d", &x.idade);
-                Insere(x, &Lista);
-                break;
-            case 4:
-                Imprime(Lista);    
-                break;    
-           default:
+        case 1:
+            FLVazia(&Lista);
+            printf("Lista esvaziada com sucesso!\n");
+            break;
+        case 2:
+            resposta = Vazia(Lista);
+            if (resposta == 1)
+                printf("A lista esta vazia!\n");
+            else
+                printf("A lista nao esta vazia!\n");
+            break;
+        case 3:
+            printf("Digite o codigo\n");
+            scanf("%d", &x.cod);
+            printf("Digite a idade\n");
+            scanf("%d", &x.idade);
+            Insere(x, &Lista);
+            break;
+        case 4:
+            Imprime(Lista);
+            break;
+        case 5:
+            printf("Funcao de pesquisa nao implementada!\n");
+            break;    
+        default:
             printf("Opcao invalida!\n");
         }
         _getch();
         system("cls");
-    }while(opcao!=0);   
+    } while (opcao != 0);
     return 0;
 }
